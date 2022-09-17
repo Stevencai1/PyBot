15ce59eabc425f092eee4e816990f20ee684266a
stevencai1 <steven30405@gmail.com>
2022-09-17 08:30:14 +0000

    discord bot create

--- pybot.py
+++ pybot.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print("Bot is Online!")
+
+bot.run("ODUzMTgxMDI2MzQwMDQ0ODAx.GSWrNt.-csLOzEW1O0CWmuXqVMEllfOiLqjtD7oIQsLBY")
