8585c64d824514d56dc2510a02c1a0790e3a1a8b
stevencai1 <steven30405@gmail.com>
2022-09-17 08:33:31 +0000

    on_member_join

--- pybot.py
+++ pybot.py
@@ -7,4 +7,8 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print("Bot is Online!")
 
+@bot.event
+async def on_member_join(member):
+    print(f'{member} Joined!')
+
 bot.run("ODUzMTgxMDI2MzQwMDQ0ODAx.GSWrNt.-csLOzEW1O0CWmuXqVMEllfOiLqjtD7oIQsLBY")
