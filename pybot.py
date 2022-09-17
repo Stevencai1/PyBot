a7c765023b6456a9f183168210e6b684628c7a5a
stevencai1 <steven30405@gmail.com>
2022-09-17 08:48:59 +0000

    ping command

--- pybot.py
+++ pybot.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='!', intents = intents)
 
 @bot.event
 async def on_ready():
@@ -18,5 +20,9 @@ async def on_member_remove(member):
     print(f'{member} Leave!')
     channel = bot.get_channel(853181663964954667)
     await channel.send(f'{member} Leave!')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)} ms')
 
 bot.run("ODUzMTgxMDI2MzQwMDQ0ODAx.GSWrNt.-csLOzEW1O0CWmuXqVMEllfOiLqjtD7oIQsLBY")
