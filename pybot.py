8afb73ea165b4ed565e07f1732f929862c68a1bc
stevencai1 <steven30405@gmail.com>
2022-09-17 08:37:20 +0000

    channel.send

--- pybot.py
+++ pybot.py
@@ -9,10 +9,14 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    print(f'{member} Joined!')
+    print(f'{member} Join!')
+    channel = bot.get_channel(853181663964954667)
+    await channel.send(f'{member} Join!')
 
 @bot.event
 async def on_member_remove(member):
-    print(f'{member} Leaved!')
+    print(f'{member} Leave!')
+    channel = bot.get_channel(853181663964954667)
+    await channel.send(f'{member} Leave!')
 
 bot.run("ODUzMTgxMDI2MzQwMDQ0ODAx.GSWrNt.-csLOzEW1O0CWmuXqVMEllfOiLqjtD7oIQsLBY")
