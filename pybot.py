914a900251d3f69f241136e323abb37c20fe250b
stevencai1 <steven30405@gmail.com>
2022-09-17 09:07:45 +0000

    setting.json

--- pybot.py
+++ pybot.py
@@ -1,9 +1,10 @@
-import discord
+import discord, json, random
 from discord.ext import commands
+with open('setting.json', 'r', encoding='utf8') as jfiles:
+    jdata = json.load(jfiles)
 
 intents = discord.Intents.all()
-
-bot = commands.Bot(command_prefix='!', intents = intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
@@ -12,17 +13,17 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     print(f'{member} Join!')
-    channel = bot.get_channel(853181663964954667)
+    channel = bot.get_channel(int(jdata['general_channel']))
     await channel.send(f'{member} Join!')
 
 @bot.event
 async def on_member_remove(member):
     print(f'{member} Leave!')
-    channel = bot.get_channel(853181663964954667)
+    channel = bot.get_channel(int(jdata['general_channel']))
     await channel.send(f'{member} Leave!')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)} ms')
 
-bot.run("ODUzMTgxMDI2MzQwMDQ0ODAx.GSWrNt.-csLOzEW1O0CWmuXqVMEllfOiLqjtD7oIQsLBY")
+bot.run(jdata['token'])
