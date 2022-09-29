a72f590e7230ed90474bf9461c7864ade3d524a6
BlackBB <steven30405@gmail.com>
2022-09-29 03:35:23 +0000

    Roll Command

--- cmds/Main.py
+++ cmds/Main.py
@@ -1,4 +1,4 @@
-import discord, json
+import discord, json, random
 from discord.ext import commands
 from core.classes import Cog_Extension
 with open('setting.json', 'r', encoding='utf8') as jfiles:
@@ -8,6 +8,10 @@ class Main(Cog_Extension):
     @commands.command()
     async def ping(self, ctx):
         await ctx.send(f'{round(self.bot.latency*1000)}ms')
+
+    @commands.command()
+    async def roll(self, ctx, rolls = 100):
+        await ctx.send(f'你擲出了**{random.randrange(1,rolls)}**點')
 
 def setup(bot):
     bot.add_cog(Main(bot))
