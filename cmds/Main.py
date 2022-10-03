0f435c89350de91e54290ec763c0f0447972b1db
BlackBB <steven30405@gmail.com>
2022-10-03 03:27:04 +0000

    Roll Update

--- cmds/Main.py
+++ cmds/Main.py
@@ -11,7 +11,18 @@ class Main(Cog_Extension):
 
     @commands.command()
     async def roll(self, ctx, rolls = 100):
-        await ctx.send(f'你擲出了**{random.randrange(1,rolls)}**點')
+        rolls_value = random.randrange(-10,rolls)
+        await ctx.send(f'你擲出了**{rolls_value}**點')
+        if rolls_value >= 90:
+            await ctx.send(f'**大吉**')
+        elif rolls_value < 90 and rolls_value > 10:
+            await ctx.send(f'中吉')
+        elif rolls_value <= 10 and rolls_value >=0:
+            await ctx.send(f'*小吉*')
+        else:
+            await ctx.send(f'~~*兇*~~')
+        if rolls_value == 100 or rolls_value == -10:
+            await ctx.send(f'__**高雄發大財**__')
 
 def setup(bot):
     bot.add_cog(Main(bot))
